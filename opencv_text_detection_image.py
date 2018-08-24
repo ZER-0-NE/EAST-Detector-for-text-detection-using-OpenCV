171ca9b77b882767b87e29fe4de1bc52ddb5b13d
Abhishek Singh <abhishek.nitdgp.98@gmail.com>
2018-08-24 18:09:56 +0530

    Detection from images completed

--- opencv_text_detection_image.py
+++ opencv_text_detection_image.py
@@ -51,6 +51,56 @@ end = time.time()
 
 print("[INFO] text detection took {:.6f} seconds".format(end-start))
 
+# grab the rows and columns from score volume
+(numrows, numcols) = scores.shape[2:4]
+rects = [] #stores the bounding box coordiantes for text regions
+confidences = [] # stores the probability associated with each bounding box region in rects
+
+for y in range(0, numrows):
+	scoresdata = scores[0,0,y]
+	xdata0 = geometry[0,0,y]
+	xdata1 = geometry[0,1,y]
+	xdata2 = geometry[0,2,y]
+	xdata3 = geometry[0,3,y]
+	anglesdata = geometry[0,4,y]
+
+	for x in range(0, numcols):
+		if scoresdata[x] <args["min_confidence"]: # if score is less than min_confidence, ignore
+			continue
+	
+	(offsetx, offsety) = (x*4.0, y*4.0) # EAST detector automatically reduces volume size as it passes through the network
+	#extracting the rotation angle for the prediction and computing their sine and cos
+
+	angle = anglesdata[2:4]
+	cos = np.cos(angle)
+	sin = np.cos(angle)
+
+	h = xdata0[x] + xdata2[x]
+	w = xdata1[x] + xdata3[x]
+
+	endx = int(offsetx + (cos * xdata1[x]) + (sin * xdata2[x]))
+	endy = int(offsety + (sin * xdata1[x]) + (cos * xdata2[x]))
+	startx = int(endx - w)
+	starty = int(endy - h)
+
+	# appending the confidence score and probabilities to list
+	recs.append((startx, starty, endx, endy))
+	confidences.append(scoresdata[x])
+
+# applying non-maxima suppression to supppress weak and overlapping bounding boxes
+boxes = non_max_suppression(np.array(rects), probs = confidences)
+
+for(startx, starty, endx, endy) in boxes:
+	startx = int(startx * rW)
+	starty = int(starty * rH)
+	endx = int(endx * rW)
+	endy = int(endy * rH)
+
+	cv2.rectangle(orig, (startx, starty), (endx, endy), (0,255,0), 2)
+
+cv2.imshow("text Detection", orig)
+cv2.waitKey(0)
+
 
 
 
