f2382c6aba13fd544b58e69132318f54aba10161
Abhishek Singh <abhishek.nitdgp.98@gmail.com>
2019-09-28 11:21:40 +0530

    working code

--- opencv_text_detection_image.py
+++ opencv_text_detection_image.py
@@ -1,105 +1,134 @@
+# USAGE
+# python opencv_text_detection_image.py --image images/lebron_james.jpg
+# --east frozen_east_text_detection.pb
+
+# import the necessary packages
+from imutils.object_detection import non_max_suppression
 import numpy as np
-import cv2
 import argparse
 import time
-from imutils.object_detection import non_max_suppression
+import cv2
 
-
+# construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", type = str,
-	 help = "path to input image")
-ap.add_argument("-east", "--east", type = str,
-	 help = "path to input EAST Detector")
-ap.add_argument("-c", "--min-confidence", type = float,
-	default = 0.5, help = "minimum probability required to inspect a region")
-ap.add_argument("-w", "--width", type = int,
-	default = 320, help = "resized image width(should be multiple of 32)")
-ap.add_argument("-e", "--height", type = int,
-	default = 320, help = "resized image height(should be multiple of 32)")
+ap.add_argument("-i", "--image", type=str,
+                help="path to input image")
+ap.add_argument("-east", "--east", type=str,
+                help="path to input EAST text detector")
+ap.add_argument("-c", "--min-confidence", type=float, default=0.5,
+                help="minimum probability required to inspect a region")
+ap.add_argument("-w", "--width", type=int, default=320,
+                help="resized image width (should be multiple of 32)")
+ap.add_argument("-e", "--height", type=int, default=320,
+                help="resized image height (should be multiple of 32)")
 args = vars(ap.parse_args())
 
+# load the input image and grab the image dimensions
 image = cv2.imread(args["image"])
 orig = image.copy()
-(h,w) = image.shape[:2]
+(H, W) = image.shape[:2]
 
-# setting new width and height
+# set the new width and height and then determine the ratio in change
+# for both the width and height
 (newW, newH) = (args["width"], args["height"])
-rW = w/float(newW)
-rH = h/float(newH)
+rW = W / float(newW)
+rH = H / float(newH)
 
-#resize the image
+# resize the image and grab the new image dimensions
 image = cv2.resize(image, (newW, newH))
-(h,w) = image.shape[:2]
+(H, W) = image.shape[:2]
 
-# we define the two output layer names for the EAST Detector model
-# that we are interested in
-layers = [
-	"feature_fusion/Conv_7/Sigmoid",
-	"feature_fusion/concat_3"]
+# define the two output layer names for the EAST detector model that
+# we are interested -- the first is the output probabilities and the
+# second can be used to derive the bounding box coordinates of text
+layerNames = [
+    "feature_fusion/Conv_7/Sigmoid",
+    "feature_fusion/concat_3"]
 
-# loading pre-trained EAST detector
-print("[INFO] loading EAST text detector")
+# load the pre-trained EAST text detector
+print("[INFO] loading EAST text detector...")
 net = cv2.dnn.readNet(args["east"])
 
-blob = cv2.dnn.blobFromImage(image, 1.0, (w,h),
-	(123.68, 116.78, 103.94), swapRB = True, crop = False)
-
+# construct a blob from the image and then perform a forward pass of
+# the model to obtain the two output layer sets
+blob = cv2.dnn.blobFromImage(image, 1.0, (W, H),
+                             (123.68, 116.78, 103.94), swapRB=True, crop=False)
 start = time.time()
 net.setInput(blob)
-(scores, geometry) = net.forward(layers)
+(scores, geometry) = net.forward(layerNames)
 end = time.time()
 
-print("[INFO] text detection took {:.6f} seconds".format(end-start))
+# show timing information on text prediction
+print("[INFO] text detection took {:.6f} seconds".format(end - start))
 
-# grab the rows and columns from score volume
-(numrows, numcols) = scores.shape[2:4]
-rects = [] #stores the bounding box coordiantes for text regions
-confidences = [] # stores the probability associated with each bounding box region in rects
+# grab the number of rows and columns from the scores volume, then
+# initialize our set of bounding box rectangles and corresponding
+# confidence scores
+(numRows, numCols) = scores.shape[2:4]
+rects = []
+confidences = []
 
-for y in range(0, numrows):
-	scoresdata = scores[0,0,y]
-	xdata0 = geometry[0,0,y]
-	xdata1 = geometry[0,1,y]
-	xdata2 = geometry[0,2,y]
-	xdata3 = geometry[0,3,y]
-	anglesdata = geometry[0,4,y]
+# loop over the number of rows
+for y in range(0, numRows):
+    # extract the scores (probabilities), followed by the geometrical
+    # data used to derive potential bounding box coordinates that
+    # surround text
+    scoresData = scores[0, 0, y]
+    xData0 = geometry[0, 0, y]
+    xData1 = geometry[0, 1, y]
+    xData2 = geometry[0, 2, y]
+    xData3 = geometry[0, 3, y]
+    anglesData = geometry[0, 4, y]
 
-	for x in range(0, numcols):
-		if scoresdata[x] <args["min_confidence"]: # if score is less than min_confidence, ignore
-			continue
-	
-	(offsetx, offsety) = (x*4.0, y*4.0) # EAST detector automatically reduces volume size as it passes through the network
-	#extracting the rotation angle for the prediction and computing their sine and cos
+    # loop over the number of columns
+    for x in range(0, numCols):
+        # if our score does not have sufficient probability, ignore it
+        if scoresData[x] < args["min_confidence"]:
+            continue
 
-	angle = anglesdata[2:4]
-	cos = np.cos(angle)
-	sin = np.cos(angle)
+        # compute the offset factor as our resulting feature maps will
+        # be 4x smaller than the input image
+        (offsetX, offsetY) = (x * 4.0, y * 4.0)
 
-	h = xdata0[x] + xdata2[x]
-	w = xdata1[x] + xdata3[x]
+        # extract the rotation angle for the prediction and then
+        # compute the sin and cosine
+        angle = anglesData[x]
+        cos = np.cos(angle)
+        sin = np.sin(angle)
 
-	endx = int(offsetx + (cos * xdata1[x]) + (sin * xdata2[x]))
-	endy = int(offsety + (sin * xdata1[x]) + (cos * xdata2[x]))
-	startx = int(endx - w)
-	starty = int(endy - h)
+        # use the geometry volume to derive the width and height of
+        # the bounding box
+        h = xData0[x] + xData2[x]
+        w = xData1[x] + xData3[x]
 
-	startx, endx = np.clip([startx, endx], 0, w)
-	starty, endy = np.clip([starty, endy], 0, h)
+        # compute both the starting and ending (x, y)-coordinates for
+        # the text prediction bounding box
+        endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
+        endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
+        startX = int(endX - w)
+        startY = int(endY - h)
 
-	# appending the confidence score and probabilities to list
-	rects.append((startx, starty, endx, endy))
-	confidences.append(scoresdata[x])
+        # add the bounding box coordinates and probability score to
+        # our respective lists
+        rects.append((startX, startY, endX, endY))
+        confidences.append(scoresData[x])
 
-# applying non-maxima suppression to supppress weak and overlapping bounding boxes
-boxes = non_max_suppression(np.array(rects), probs = confidences)
+# apply non-maxima suppression to suppress weak, overlapping bounding
+# boxes
+boxes = non_max_suppression(np.array(rects), probs=confidences)
 
-for(startx, starty, endx, endy) in boxes:
-	startx = int(startx * rW)
-	starty = int(starty * rH)
-	endx = int(endx * rW)
-	endy = int(endy * rH)
+# loop over the bounding boxes
+for (startX, startY, endX, endY) in boxes:
+    # scale the bounding box coordinates based on the respective
+    # ratios
+    startX = int(startX * rW)
+    startY = int(startY * rH)
+    endX = int(endX * rW)
+    endY = int(endY * rH)
 
-	cv2.rectangle(orig, (startx, starty), (endx, endy), (0,255,0), 2)
+    # draw the bounding box on the image
+    cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 255, 0), 2)
 
-cv2.imshow("text Detection", orig)
+# show the output image
+cv2.imshow("Text Detection", orig)
 cv2.waitKey(0)
